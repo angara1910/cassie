eb9782a4d2b57d01f868b9b589ac80b8a386c1dc
CASSIExCFX <57040969+CASSIExCFX@users.noreply.github.com>
2019-11-24 16:01:38 +0300

    Create diaçıkar.js

---
+++ diaçıkar.js
@@ -0,0 +1,22 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args) => {
+ 
+let aydi = args[0]
+let dia = message.guild.members.get(args[0]);
+let cfxz = message.guild.roles.find(cfxz => cfxz.name === "Diamond");
+  
+  const cfx = new Discord.RichEmbed()
+  .setColor("#c987ff")
+  .setDescription(`**${dia}** artık **[ Diamond üye ]** değil!`)
+  .setImage(`https://cdn.discordapp.com/attachments/647135800420466688/647376223646777374/giphy.gif`)
+      
+  if (!aydi) return message.channel.send('Bir kullanıcının IDsini girmelisin?')
+  
+  db.delete(`cfxz${aydi}`)
+  await(dia.removeRole(cfxz.id));
+  message.channel.send(cfx)
+}
+exports.conf = { enabled: true, guildOnly: false, aliases: [], permLevel: 4 };
+exports.help = { name: 'diaçıkar' };
